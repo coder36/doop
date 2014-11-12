e7a6839b46dc3278478d0dc7e1e1e328fd005ce6
Mark Middleton <markymiddleton@gmail.com>
2014-11-12 14:24:22 +0000

    Fixed path issues and set version numbers for generator

--- lib/generators/doopgovuk/doopgovuk_generator.rb
+++ lib/generators/doopgovuk/doopgovuk_generator.rb
@@ -1,5 +1,5 @@
 class DoopgovukGenerator < Rails::Generators::Base
-  source_root File.expand_path('../../../demo', __FILE__)
+  source_root File.expand_path('../../../../demo', __FILE__)
   argument :name, :type => :string, :default => "demo"
 
 
@@ -11,6 +11,8 @@ class DoopgovukGenerator < Rails::Generators::Base
     copy_file "app/assets/javascripts/demo/analytics.js.erb", "app/assets/javascripts/#{name}/analytics.js.erb"
     template "app/controllers/demo_controller.rb", "app/controllers/#{name}_controller.rb"
     copy_file "app/views/layouts/application.html.erb",  "app/views/layouts/application.html.erb"
+    directory "public/uploads"
+    copy_file "public/uploads/.keep",  "public/uploads/.keep"
     directory "app/views/doop"
     directory "app/views/demo", "app/views/#{name}"
     gsub_file "app/controllers/#{name}_controller.rb", /DemoController/, "#{name.capitalize}Controller"
@@ -27,22 +29,27 @@ class DoopgovukGenerator < Rails::Generators::Base
     route "get '#{name}/harness'"
     route "post '#{name}/harness'"
 
-    gem "govuk_frontend_toolkit"
-    gem "govuk_template"
-    gem "jquery-ui-rails"
-    gem "blueimp-gallery"
-    gem "jquery-fileupload-rails"
+    gem "govuk_frontend_toolkit", "~> 1.6"
+    gem "govuk_template", "~> 0.9"
+    gem "jquery-ui-rails", "~> 5.0"
+    gem "blueimp-gallery", "~> 2.11.0"
+    gem "jquery-fileupload-rails", "~> 0.4"
+
+
+    gem_group :production do
+      gem 'rails_12factor', "~> 0.0"
+      gem 'pg', "~> 0.17"
+    end
 
     gem_group :development do
-      gem 'rspec-rails'
-      gem 'guard-rspec'
-      gem 'capybara'
-      gem 'pry'
-      gem 'pry-nav'
-      gem 'capybara-webkit'
-      gem 'headless'
-      gem 'capybara-screenshot'
-      gem 'selenium-webdriver'
+      gem 'rspec-rails', "~> 3.1"
+      gem 'guard-rspec', "~> 4.3"
+      gem 'capybara', "~> 2.4"
+      gem 'pry', "~> 0.10"
+      gem 'pry-nav', "~> 0.2"
+      gem 'capybara-webkit', "~> 1.3"
+      gem 'headless', "~> 1.0"
+      gem 'capybara-screenshot', "~> 1.0"
     end
 
     Bundler.with_clean_env do
